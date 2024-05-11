1bbadbd3d9ebcd14c825281074d19dafc926639e
Cequallium <85212821+Cequallium@users.noreply.github.com>
2024-05-11 16:34:53 +0530

    Fix formatting

--- main.go
+++ main.go
@@ -10,22 +10,22 @@ import (
 const Port, Path, Name = ":8080", "/", "Cequallium"
 const separator = "_____________________________________________________________"
 
-var Footer = fmt.Sprintf("%v\n\n Probably best viewed in a terminal...\n\nCopyright (c) %v %v. All Rights Reserved.", separator, time.Now().Year(), Name)
+var Footer = fmt.Sprintf("%v\n\n Probably best viewed in a terminal...\n\nCopyright (c) %v %v. All Rights Reserved.\n", separator, time.Now().Year(), Name)
 
 func serve(w http.ResponseWriter, r *http.Request) {
 	wd, _ := os.Getwd()
 	file, err := os.ReadFile(fmt.Sprintf("%v/pages%v", wd, r.URL.Path))
 
 	if err != nil {
-		fmt.Fprintf(w, "404")
+		fmt.Fprintf(w, "404\n")
 	} else {
 		fmt.Fprintf(w, "[ %v ]\n\n", r.URL.Path)
-		fmt.Fprintf(w, "%v", string(file))
+		fmt.Fprintf(w, "%v\n", string(file))
 		fmt.Fprint(w, Footer)
 	}
 }
 func main() {
-	fmt.Printf("whispering on localhost%v%v", Port, Path)
+  fmt.Printf("whispering on https://localhost%v%v\n", Port, Path)
 	http.HandleFunc(Path, serve)
 	http.ListenAndServe(Port, nil)
 }
