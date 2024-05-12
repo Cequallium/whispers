bab1a08edc77ed00ce5fe1e4118979a2f1135521
Cequallium <85212821+Cequallium@users.noreply.github.com>
2024-05-12 08:21:42 +0530

    Further configuration options + Avoid self contradiction

---
+++ README.txt
@@ -0,0 +1,10 @@
+[ whisper ]
+
+    an ode to plaintext
+    a breeding ground for exploits
+    a plaintext file server
+    fit for whispers from the void
+
+_____________________________________________________________
+
+Copyright (c) 2024 Cequallium. All Rights Reserved.
--- main.go
+++ main.go
@@ -4,28 +4,30 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
-const Port, Path, Name = ":8080", "/", "Cequallium"
+const Port, UrlPath, Name, PagePath = ":8080", "/", "Cequallium", "pages"
 const separator = "_____________________________________________________________"
 
 var Footer = fmt.Sprintf("%v\n\n Probably best viewed in a terminal...\n\nCopyright (c) %v %v. All Rights Reserved.\n", separator, time.Now().Year(), Name)
 
 func serve(w http.ResponseWriter, r *http.Request) {
 	wd, _ := os.Getwd()
-	file, err := os.ReadFile(fmt.Sprintf("%v/pages%v", wd, r.URL.Path))
+	file, err := os.ReadFile(fmt.Sprintf("%v/%v%v", wd, PagePath, r.URL.Path))
 
 	if err != nil {
 		fmt.Fprintf(w, "404\n")
+		fmt.Println(r)
 	} else {
-		fmt.Fprintf(w, "[ %v ]\n\n", r.URL.Path)
-		fmt.Fprintf(w, "%v\n", string(file))
-		fmt.Fprint(w, Footer)
+		fmt.Fprintf(w, "[ %v ]\n\n", strings.TrimPrefix(r.URL.Path, "/"))
+		fmt.Fprint(w, string(file))
+		fmt.Fprintf(w, "%v\n", Footer)
 	}
 }
 func main() {
-  fmt.Printf("whispering on https://localhost%v%v\n", Port, Path)
-	http.HandleFunc(Path, serve)
+	fmt.Printf("whispering on http://localhost%v%v\n", Port, UrlPath)
+	http.HandleFunc(UrlPath, serve)
 	http.ListenAndServe(Port, nil)
 }
