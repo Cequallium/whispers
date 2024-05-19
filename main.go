cb1d17cfbc93e66cb118292825edc598a98c0a8c
Cequallium <85212821+Cequallium@users.noreply.github.com>
2024-05-19 20:18:11 +0530

    Final

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 	"strings"
 	"time"
 )
+
 // files will be served in ./pages/
 const Port, UrlPath, Name, PagePath = ":8080", "/", "Author", "pages"
-const separator = "_____________________________________________________________"
 
-var Footer = fmt.Sprintf("%v\n\n Probably best viewed in a terminal...\n\nCopyright (c) %v %v. All Rights Reserved.\n", separator, time.Now().Year(), Name)
+var separator string = strings.Repeat("_", 60)
+
+var Footer = fmt.Sprintf(
+	"%v\n\n Probably best viewed in a terminal...\n\nCopyright (c) %v %v. All Rights Reserved.\n",
+	separator,
+	time.Now().Year(),
+	Name,
+)
 
 func serve(w http.ResponseWriter, r *http.Request) {
 	wd, _ := os.Getwd()
@@ -19,13 +26,13 @@ func serve(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		fmt.Fprintf(w, "404\n")
-		fmt.Println(r)
 	} else {
 		fmt.Fprintf(w, "[ %v ]\n\n", strings.TrimPrefix(r.URL.Path, "/"))
 		fmt.Fprint(w, string(file))
 		fmt.Fprintf(w, "%v\n", Footer)
 	}
 }
+
 func main() {
 	fmt.Printf("whispering on http://localhost%v%v\n", Port, UrlPath)
 	http.HandleFunc(UrlPath, serve)
